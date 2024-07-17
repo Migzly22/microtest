99dacecc44765852b07fdeb5f75da46714f704bd
Rolly Migriño <rolly.migrino02@gmail.com>
2024-07-18 07:35:35 +0800

    Testing

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname));
 
 // Serve the index.html file on the root path
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname,'public', 'index.html'));
 });
 
 // Start the server
